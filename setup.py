b1ac89c02a08dd9b1ff1b000b6a13dff5df249d1
larkee <larkee@users.noreply.github.com>
2020-08-17 20:08:02 +1000

    style: fix lint

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ extras = {
     "tracing": [
         "opentelemetry-api==0.11b0",
         "opentelemetry-sdk==0.11b0",
-        "opentelemetry-instrumentation==0.11b0"
+        "opentelemetry-instrumentation==0.11b0",
     ]
 }
 
