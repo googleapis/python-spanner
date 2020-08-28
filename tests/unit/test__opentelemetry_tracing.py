f1db0cc6dfefa457c381d9cb70861598a6330dde
larkee <larkee@users.noreply.github.com>
2020-08-28 17:23:42 +1000

    test: update test__opentelemetry_tracing.py

--- tests/unit/test__opentelemetry_tracing.py
+++ tests/unit/test__opentelemetry_tracing.py
@@ -57,8 +57,8 @@ if HAS_OPENTELEMETRY_INSTALLED:
 
             expected_attributes = {
                 "db.type": "spanner",
-                "db.url": "spanner.googleapis.com:443",
-                "net.host.name": "spanner.googleapis.com:443",
+                "db.url": "spanner.googleapis.com",
+                "net.host.name": "spanner.googleapis.com",
             }
             expected_attributes.update(extra_attributes)
 
@@ -82,8 +82,8 @@ if HAS_OPENTELEMETRY_INSTALLED:
 
             expected_attributes = {
                 "db.type": "spanner",
-                "db.url": "spanner.googleapis.com:443",
-                "net.host.name": "spanner.googleapis.com:443",
+                "db.url": "spanner.googleapis.com",
+                "net.host.name": "spanner.googleapis.com",
             }
             expected_attributes.update(extra_attributes)
 
@@ -99,7 +99,7 @@ if HAS_OPENTELEMETRY_INSTALLED:
             self.assertEqual(len(span_list), 1)
             span = span_list[0]
             self.assertEqual(span.kind, trace_api.SpanKind.CLIENT)
-            self.assertEqual(span.attributes, expected_attributes)
+            self.assertEqual(dict(span.attributes), expected_attributes)
             self.assertEqual(span.name, "CloudSpanner.Test")
             self.assertEqual(
                 span.status.canonical_code, StatusCanonicalCode.INVALID_ARGUMENT
