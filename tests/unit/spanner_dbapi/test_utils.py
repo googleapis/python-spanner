38cfd7382bdee7c59f849e16a1b75214af8e1690
STATION\MF <mf2199@columbia.edu>
2020-10-30 22:36:58 -0400

    chore:

--- tests/unit/spanner_dbapi/test_utils.py
+++ tests/unit/spanner_dbapi/test_utils.py
@@ -1,10 +1,9 @@
+# coding=utf-8
 # Copyright 2020 Google LLC
 #
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file or at
 # https://developers.google.com/open-source/licenses/bsd
-
-# coding=utf-8
 
 import unittest
 
