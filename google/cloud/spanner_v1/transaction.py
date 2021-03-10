825709dacbf72e4ce9a3303d9d3eb782af7e7273
Vikash Singh <3116482+vi3k6i5@users.noreply.github.com>
2021-03-10 14:14:46 +0530

    feat: undo deleted line after license text

--- google/cloud/spanner_v1/transaction.py
+++ google/cloud/spanner_v1/transaction.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Spanner read-write transaction support."""
 
 from google.protobuf.struct_pb2 import Struct
