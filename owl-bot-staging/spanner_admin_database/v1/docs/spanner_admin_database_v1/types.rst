d5709ebac1c0ea063a9ce97f47707f929f31c7fd
Anthonios Partheniou <partheniou@google.com>
2021-04-25 20:23:43 -0400

    chore: copy files from googleapis-gen b154da710c5c9eedee127c07f74b6158c9c22382

---
+++ owl-bot-staging/spanner_admin_database/v1/docs/spanner_admin_database_v1/types.rst
@@ -0,0 +1,7 @@
+Types for Google Cloud Spanner Admin Database v1 API
+====================================================
+
+.. automodule:: google.cloud.spanner_admin_database_v1.types
+    :members:
+    :undoc-members:
+    :show-inheritance:
---
+++ owl-bot-staging/spanner_admin_database/v1/google/cloud/spanner_admin_database/__init__.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from google.cloud.spanner_admin_database_v1.services.database_admin.async_client import DatabaseAdminAsyncClient
+from google.cloud.spanner_admin_database_v1.services.database_admin.client import DatabaseAdminClient
+from google.cloud.spanner_admin_database_v1.types.backup import Backup
+from google.cloud.spanner_admin_database_v1.types.backup import BackupInfo
+from google.cloud.spanner_admin_database_v1.types.backup import CreateBackupEncryptionConfig
+from google.cloud.spanner_admin_database_v1.types.backup import CreateBackupMetadata
+from google.cloud.spanner_admin_database_v1.types.backup import CreateBackupRequest
+from google.cloud.spanner_admin_database_v1.types.backup import DeleteBackupRequest
+from google.cloud.spanner_admin_database_v1.types.backup import GetBackupRequest
+from google.cloud.spanner_admin_database_v1.types.backup import ListBackupOperationsRequest
+from google.cloud.spanner_admin_database_v1.types.backup import ListBackupOperationsResponse
+from google.cloud.spanner_admin_database_v1.types.backup import ListBackupsRequest
+from google.cloud.spanner_admin_database_v1.types.backup import ListBackupsResponse
+from google.cloud.spanner_admin_database_v1.types.backup import UpdateBackupRequest
+from google.cloud.spanner_admin_database_v1.types.common import EncryptionConfig
+from google.cloud.spanner_admin_database_v1.types.common import EncryptionInfo
+from google.cloud.spanner_admin_database_v1.types.common import OperationProgress
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import CreateDatabaseMetadata
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import CreateDatabaseRequest
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import Database
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import DropDatabaseRequest
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import GetDatabaseDdlRequest
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import GetDatabaseDdlResponse
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import GetDatabaseRequest
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import ListDatabaseOperationsRequest
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import ListDatabaseOperationsResponse
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import ListDatabasesRequest
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import ListDatabasesResponse
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import OptimizeRestoredDatabaseMetadata
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import RestoreDatabaseEncryptionConfig
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import RestoreDatabaseMetadata
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import RestoreDatabaseRequest
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import RestoreInfo
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import RestoreSourceType
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import UpdateDatabaseDdlMetadata
+from google.cloud.spanner_admin_database_v1.types.spanner_database_admin import UpdateDatabaseDdlRequest
+
+__all__ = (
+    'Backup',
+    'BackupInfo',
+    'CreateBackupEncryptionConfig',
+    'CreateBackupMetadata',
+    'CreateBackupRequest',
+    'CreateDatabaseMetadata',
+    'CreateDatabaseRequest',
+    'Database',
+    'DatabaseAdminAsyncClient',
+    'DatabaseAdminClient',
+    'DeleteBackupRequest',
+    'DropDatabaseRequest',
+    'EncryptionConfig',
+    'EncryptionInfo',
+    'GetBackupRequest',
+    'GetDatabaseDdlRequest',
+    'GetDatabaseDdlResponse',
+    'GetDatabaseRequest',
+    'ListBackupOperationsRequest',
+    'ListBackupOperationsResponse',
+    'ListBackupsRequest',
+    'ListBackupsResponse',
+    'ListDatabaseOperationsRequest',
+    'ListDatabaseOperationsResponse',
+    'ListDatabasesRequest',
+    'ListDatabasesResponse',
+    'OperationProgress',
+    'OptimizeRestoredDatabaseMetadata',
+    'RestoreDatabaseEncryptionConfig',
+    'RestoreDatabaseMetadata',
+    'RestoreDatabaseRequest',
+    'RestoreInfo',
+    'RestoreSourceType',
+    'UpdateBackupRequest',
+    'UpdateDatabaseDdlMetadata',
+    'UpdateDatabaseDdlRequest',
+)
