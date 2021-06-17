b6923604ff5087e18ff4ea18e87f4ef5075d22cd
IlyaFaer <ilya.faer@mail.ru>
2021-06-17 11:22:52 +0300

    fix(db_api): use sqlparse to split DDL statements

--- tests/unit/spanner_dbapi/test_cursor.py
+++ tests/unit/spanner_dbapi/test_cursor.py
@@ -939,9 +939,16 @@ class TestCursor(unittest.TestCase):
         from google.cloud.spanner_dbapi.connection import connect
 
         EXP_DDLS = [
-            "CREATE TABLE table_name (row_id INT64) PRIMARY KEY ()",
-            "DROP INDEX index_name",
-            "DROP TABLE table_name",
+            "CREATE TABLE table_name (row_id INT64) PRIMARY KEY ();",
+            "DROP INDEX index_name;",
+            (
+                "CREATE TABLE papers ("
+                "\n    id INT64,"
+                "\n    authors ARRAY<STRING(100)>,"
+                '\n    author_list STRING(MAX) AS (ARRAY_TO_STRING(authors, ";")) stored'
+                ") PRIMARY KEY (id);"
+            ),
+            "DROP TABLE table_name;",
         ]
 
         with mock.patch(
@@ -956,7 +963,12 @@ class TestCursor(unittest.TestCase):
         cursor.execute(
             "CREATE TABLE table_name (row_id INT64) PRIMARY KEY ();"
             "DROP INDEX index_name;\n"
-            "DROP TABLE table_name;"
+            "CREATE TABLE papers ("
+            "\n    id INT64,"
+            "\n    authors ARRAY<STRING(100)>,"
+            '\n    author_list STRING(MAX) AS (ARRAY_TO_STRING(authors, ";")) stored'
+            ") PRIMARY KEY (id);"
+            "DROP TABLE table_name;",
         )
 
         self.assertEqual(connection._ddl_statements, EXP_DDLS)
