e80e30da8a080be5b9ba668e6ec206b0e6f89b5e
IlyaFaer <ilya.faer@mail.ru>
2021-11-02 11:12:09 +0300

    lint fix

--- google/cloud/spanner_dbapi/cursor.py
+++ google/cloud/spanner_dbapi/cursor.py
@@ -254,10 +254,9 @@ class Cursor(object):
                     ResultsChecksum(),
                     class_ == parse_utils.STMT_INSERT,
                 )
-                (
-                    self._result_set,
-                    self._checksum,
-                ) = self.connection.run_statement(statement)
+                (self._result_set, self._checksum,) = self.connection.run_statement(
+                    statement
+                )
                 while True:
                     try:
                         self._itr = PeekIterator(self._result_set)
