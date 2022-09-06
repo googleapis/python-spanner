c986033ff5baea49c4285a808fac7f46c6e4837d
Anthonios Partheniou <partheniou@google.com>
2022-09-06 10:09:10 -0400

    revert

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ version = "3.20.0"
 # 'Development Status :: 5 - Production/Stable'
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >=2.10, <3.0.0dev, !=2.0, !=2.1, !=2.2, !=2.3, !=2.4, !=2.5, !=2.6, !=2.7",
+    "google-api-core[grpc] >= 1.32.0, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*",
     "google-cloud-core >= 1.4.1, < 3.0dev",
     "grpc-google-iam-v1 >= 0.12.4, <1.0.0dev",
     "proto-plus >= 1.22.0, <2.0.0dev",
