2b87cb05856581209b8c8516b26dac028b1d0745
Astha Mohta <asthamohta@google.com>
2022-10-04 13:07:03 +0530

    linting

--- samples/samples/snippets_test.py
+++ samples/samples/snippets_test.py
@@ -99,8 +99,9 @@ def default_leader():
 def user_managed_instance_config_name(spanner_client):
     name = f"custom-python-samples-config-{uuid.uuid4().hex[:10]}"
     yield name
-    snippets.delete_instance_config("{}/instanceConfigs/{}".format(
-        spanner_client.project_name, name))
+    snippets.delete_instance_config(
+        "{}/instanceConfigs/{}".format(spanner_client.project_name, name)
+    )
     return
 
 
@@ -128,7 +129,8 @@ def test_create_database_explicit(sample_instance, create_database_id):
 def test_create_instance_with_processing_units(capsys, lci_instance_id):
     processing_units = 500
     retry_429(snippets.create_instance_with_processing_units)(
-        lci_instance_id, processing_units,
+        lci_instance_id,
+        processing_units,
     )
     out, _ = capsys.readouterr()
     assert lci_instance_id in out
@@ -163,8 +165,12 @@ def test_list_instance_config(capsys):
 
 
 @pytest.mark.dependency(name="create_instance_config")
-def test_create_instance_config(capsys, user_managed_instance_config_name, base_instance_config_id):
-    snippets.create_instance_config(user_managed_instance_config_name, base_instance_config_id)
+def test_create_instance_config(
+    capsys, user_managed_instance_config_name, base_instance_config_id
+):
+    snippets.create_instance_config(
+        user_managed_instance_config_name, base_instance_config_id
+    )
     out, _ = capsys.readouterr()
     assert "Created instance configuration" in out
 
@@ -179,8 +185,11 @@ def test_update_instance_config(capsys, user_managed_instance_config_name):
 @pytest.mark.dependency(depends=["create_instance_config"])
 def test_delete_instance_config(capsys, user_managed_instance_config_name):
     spanner_client = spanner.Client()
-    snippets.delete_instance_config("{}/instanceConfigs/{}".format(
-        spanner_client.project_name, user_managed_instance_config_name))
+    snippets.delete_instance_config(
+        "{}/instanceConfigs/{}".format(
+            spanner_client.project_name, user_managed_instance_config_name
+        )
+    )
     out, _ = capsys.readouterr()
     assert "successfully deleted" in out
 
@@ -543,7 +552,8 @@ def test_create_table_with_datatypes(capsys, instance_id, sample_database):
 
 
 @pytest.mark.dependency(
-    name="insert_datatypes_data", depends=["create_table_with_datatypes"],
+    name="insert_datatypes_data",
+    depends=["create_table_with_datatypes"],
 )
 def test_insert_datatypes_data(capsys, instance_id, sample_database):
     snippets.insert_datatypes_data(instance_id, sample_database.database_id)
@@ -605,7 +615,8 @@ def test_query_data_with_string(capsys, instance_id, sample_database):
 
 
 @pytest.mark.dependency(
-    name="add_numeric_column", depends=["create_table_with_datatypes"],
+    name="add_numeric_column",
+    depends=["create_table_with_datatypes"],
 )
 def test_add_numeric_column(capsys, instance_id, sample_database):
     snippets.add_numeric_column(instance_id, sample_database.database_id)
@@ -628,7 +639,8 @@ def test_query_data_with_numeric_parameter(capsys, instance_id, sample_database)
 
 
 @pytest.mark.dependency(
-    name="add_json_column", depends=["create_table_with_datatypes"],
+    name="add_json_column",
+    depends=["create_table_with_datatypes"],
 )
 def test_add_json_column(capsys, instance_id, sample_database):
     snippets.add_json_column(instance_id, sample_database.database_id)
