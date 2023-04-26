de7673489657f1f5f30227473b335a1e7c29b7fd
Astha Mohta <35952883+asthamohta@users.noreply.github.com>
2023-04-26 14:41:34 +0530

    Update connection.py

--- google/cloud/spanner_dbapi/connection.py
+++ google/cloud/spanner_dbapi/connection.py
@@ -497,7 +497,7 @@ def connect(
     credentials=None,
     pool=None,
     user_agent=None,
-    route_to_leader_enabled=True,
+    route_to_leader_enabled=False,
 ):
     """Creates a connection to a Google Cloud Spanner database.
 
@@ -532,7 +532,7 @@ def connect(
 
     :type route_to_leader_enabled: boolean
     :param route_to_leader_enabled:
-        (Optional) Default True. Set route_to_leader_enabled as False to
+        (Optional) Default False. Set route_to_leader_enabled as False to
         Disable leader aware routing. Disabling leader aware routing would
         route all requests in RW/PDML transactions to any region.
 
