51e21e94d61eec853f6bafad2ba77562d1511f6b
Astha Mohta <35952883+asthamohta@users.noreply.github.com>
2023-07-13 11:11:53 +0530

    Update samples/samples/snippets.py
    Co-authored-by: Vishwaraj Anand <vishwaraj.anand00@gmail.com>

--- samples/samples/snippets.py
+++ samples/samples/snippets.py
@@ -2522,7 +2522,7 @@ def alter_table_with_foreign_key_delete_cascade(instance_id, database_id):
 
 # [START spanner_drop_foreign_key_constraint_delete_cascade]
 def drop_foreign_key_constraint_delete_cascade(instance_id, database_id):
-    """Creates a table with foreign key delete cascade action"""
+    """Alter table to drop foreign key delete cascade action"""
     spanner_client = spanner.Client()
     instance = spanner_client.instance(instance_id)
     database = instance.database(database_id)
