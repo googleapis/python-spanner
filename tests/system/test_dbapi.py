d71b53a9eda1ee06ba0a9aec5403174740c84508
ankiaga <ankiaga@google.com>
2023-11-08 15:00:09 +0530

    Fixing test

--- tests/system/test_dbapi.py
+++ tests/system/test_dbapi.py
@@ -347,38 +347,45 @@ def test_DDL_autocommit(shared_instance, dbapi_database):
 def test_ddl_executemany(shared_instance, dbapi_database):
     """Check that DDLs in autocommit mode are immediately executed."""
 
-    conn = Connection(shared_instance, dbapi_database)
-    want_row = (
-        1,
-        "first-name",
-    )
-    cur = conn.cursor()
-    cur.execute(
+    try:
+        conn = Connection(shared_instance, dbapi_database)
+        want_row = (
+            1,
+            "first-name",
+        )
+        cur = conn.cursor()
+        cur.execute(
+            """
+            CREATE TABLE Singers (
+                SingerId     INT64 NOT NULL,
+                Name    STRING(1024),
+            ) PRIMARY KEY (SingerId)
         """
-        CREATE TABLE Singers (
-            SingerId     INT64 NOT NULL,
-            Name    STRING(1024),
-        ) PRIMARY KEY (SingerId)
-    """
-    )
-    cur.executemany(
-        """
-INSERT INTO Singers (SingerId, Name)
-VALUES (%s, %s)
-""",
-        [want_row],
-    )
-    conn.commit()
+        )
+        cur.executemany(
+            """
+    INSERT INTO Singers (SingerId, Name)
+    VALUES (%s, %s)
+    """,
+            [want_row],
+        )
+        conn.commit()
 
-    # read the resulting data from the database
-    cur.execute("SELECT * FROM Singers")
-    got_rows = cur.fetchall()
-    conn.commit()
+        # read the resulting data from the database
+        cur.execute("SELECT * FROM Singers")
+        got_rows = cur.fetchall()
 
-    assert got_rows == [want_row]
+        assert got_rows == [want_row]
 
-    cur.close()
-    conn.close()
+        cur.close()
+        conn.close()
+
+    finally:
+        # Delete table
+        table = dbapi_database.table("Singers")
+        if table.exists():
+            op = dbapi_database.update_ddl(["DROP TABLE Singers"])
+            op.result()
 
 
 @pytest.mark.skipif(_helpers.USE_EMULATOR, reason="Emulator does not support json.")
