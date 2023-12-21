d35e7af2eedafc184dd164e166b8a2789d4d180e
Sri Harsha CH <57220027+harshachinta@users.noreply.github.com>
2023-12-21 23:49:13 +0530

    chore: revert

--- samples/samples/noxfile.py
+++ samples/samples/noxfile.py
@@ -239,7 +239,7 @@ def _session_tests(
     )
 
 
-@nox.session(python=TESTED_VERSIONS)
+@nox.session(python=ALL_VERSIONS)
 def py(session: nox.sessions.Session) -> None:
     """Runs py.test for a sample using the specified version of Python."""
     if session.python in TESTED_VERSIONS:
