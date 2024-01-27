d486b13c1615df1c70645ca0a399bdaace5f1496
Sri Harsha CH <sriharshach@google.com>
2024-01-27 11:33:14 +0000

    chore(spanner): fix formatting

--- samples/samples/admin/samples_test.py
+++ samples/samples/admin/samples_test.py
@@ -19,61 +19,35 @@ Spanner.
 For more information, see the README.rst under /spanner.
 """
 
-
 import uuid
 
-
 from google.api_core import exceptions
-
 from google.cloud.spanner_admin_database_v1.types.common import DatabaseDialect
-
 import pytest
-
 from test_utils.retry import RetryErrors
-
 
 import samples
 
-
 CREATE_TABLE_SINGERS = """\
-
 CREATE TABLE Singers (
-
     SingerId     INT64 NOT NULL,
-
     FirstName    STRING(1024),
-
     LastName     STRING(1024),
-
     SingerInfo   BYTES(MAX),
-
     FullName     STRING(2048) AS (
-
         ARRAY_TO_STRING([FirstName, LastName], " ")
-
     ) STORED
-
 ) PRIMARY KEY (SingerId)
-
 """
-
 
 CREATE_TABLE_ALBUMS = """\
-
 CREATE TABLE Albums (
-
     SingerId     INT64 NOT NULL,
-
     AlbumId      INT64 NOT NULL,
-
     AlbumTitle   STRING(MAX)
-
 ) PRIMARY KEY (SingerId, AlbumId),
-
 INTERLEAVE IN PARENT Singers ON DELETE CASCADE
-
 """
-
 
 retry_429 = RetryErrors(exceptions.ResourceExhausted, delay=15)
 
@@ -87,28 +61,21 @@ def sample_name():
 def database_dialect():
     """Spanner dialect to be used for this sample.
 
-
-
     The dialect is used to initialize the dialect for the database.
-
     It can either be GoogleStandardSql or PostgreSql.
-
     """
-
     return DatabaseDialect.GOOGLE_STANDARD_SQL
 
 
 @pytest.fixture(scope="module")
 def create_instance_id():
     """Id for the low-cost instance."""
-
     return f"create-instance-{uuid.uuid4().hex[:10]}"
 
 
 @pytest.fixture(scope="module")
 def lci_instance_id():
     """Id for the low-cost instance."""
-
     return f"lci-instance-{uuid.uuid4().hex[:10]}"
 
 
@@ -136,19 +103,14 @@ def default_leader_database_id():
 def database_ddl():
     """Sequence of DDL statements used to set up the database.
 
-
-
     Sample testcase modules can override as needed.
-
     """
-
     return [CREATE_TABLE_SINGERS, CREATE_TABLE_ALBUMS]
 
 
 @pytest.fixture(scope="module")
 def default_leader():
     """Default leader for multi-region instances."""
-
     return "us-east4"
 
 
@@ -159,13 +121,9 @@ def base_instance_config_id(spanner_client):
 
 def test_create_instance_explicit(spanner_client, create_instance_id):
     # Rather than re-use 'sample_isntance', we create a new instance, to
-
     # ensure that the 'create_instance' snippet is tested.
-
     retry_429(samples.create_instance)(create_instance_id)
-
     instance = spanner_client.instance(create_instance_id)
-
     retry_429(instance.delete)()
 
 
@@ -177,13 +135,9 @@ def test_create_database_with_default_leader(
     default_leader,
 ):
     retry_429 = RetryErrors(exceptions.ResourceExhausted, delay=15)
-
     retry_429(samples.create_database_with_default_leader)(
         multi_region_instance_id, default_leader_database_id, default_leader
     )
-
     out, _ = capsys.readouterr()
-
     assert default_leader_database_id in out
-
     assert default_leader in out
