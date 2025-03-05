9a384b8fd3f0fa42e2895a43ae0944ef4f8285a4
Anthonios Partheniou <partheniou@google.com>
2025-03-05 11:09:58 +0000

    3.0->3.0.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ else:
 
 dependencies = [
     "google-api-core[grpc] >= 1.34.0, <3.0.0,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*,!=2.8.*,!=2.9.*,!=2.10.*",
-    "google-cloud-core >= 1.4.4, < 3.0",
+    "google-cloud-core >= 1.4.4, < 3.0.0",
     "grpc-google-iam-v1 >= 0.12.4, <1.0.0",
     "proto-plus >= 1.22.0, <2.0.0",
     "sqlparse >= 0.4.4",
