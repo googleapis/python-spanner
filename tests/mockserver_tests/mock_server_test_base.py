121c2cca690ef200687792cca1935453e65b117b
Emmanuel T Odeke <emmanuel@orijtech.com>
2025-05-19 11:29:27 -0700

    Fix mismatched properties for checking grpc exceptions

--- tests/mockserver_tests/mock_server_test_base.py
+++ tests/mockserver_tests/mock_server_test_base.py
@@ -61,7 +61,7 @@ def aborted_status() -> _Status:
 def unavailable_status() -> _Status:
     error = status_pb2.Status(
         code=code_pb2.UNAVAILABLE,
-        message="Received unexpected EOS on DATA frame from server",
+        message="Service unavailable.",
     )
     retry_info = RetryInfo(retry_delay=Duration(seconds=0, nanos=1))
     status = _Status(
@@ -78,23 +78,16 @@ def unavailable_status() -> _Status:
     return status
 
 
-# Creates an INTERNAL status with the smallest possible retry delay.
+# Creates an INTERNAL status.
 def internal_status() -> _Status:
     error = status_pb2.Status(
         code=code_pb2.INTERNAL,
-        message="Service unavailable.",
+        message="Received unexpected EOS on DATA frame from server",
     )
-    retry_info = RetryInfo(retry_delay=Duration(seconds=0, nanos=1))
     status = _Status(
         code=code_to_grpc_status_code(error.code),
         details=error.message,
-        trailing_metadata=(
-            ("grpc-status-details-bin", error.SerializeToString()),
-            (
-                "google.rpc.retryinfo-bin",
-                retry_info.SerializeToString(),
-            ),
-        ),
+        trailing_metadata=(("grpc-status-details-bin", error.SerializeToString()),),
     )
     return status
 
