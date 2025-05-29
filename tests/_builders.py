c8e7a1cd050847ae84bdab9882df67fb4251e3bd
Taylor Curran <taylor.curran@improving.com>
2025-05-29 12:41:52 -0700

    Update `SessionOptions` to support `GOOGLE_CLOUD_SPANNER_FORCE_DISABLE_MULTIPLEXED_SESSIONS` and add unit tests.
    Signed-off-by: Taylor Curran <taylor.curran@improving.com>

---
+++ tests/_builders.py
@@ -0,0 +1,22 @@
+# Copyright 2025 Google LLC All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from mock import create_autospec
+
+
+def build_logger():
+    """Builds and returns a logger for testing."""
+    from logging import Logger
+
+    return create_autospec(Logger, instance=True)
