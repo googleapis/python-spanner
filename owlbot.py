13e81f66a94aed87db9446e0cf1ddcfffcec3c7e
ohmayr <omairn@google.com>
2025-11-05 18:38:04 +0000

    update blacken

--- owlbot.py
+++ owlbot.py
@@ -337,4 +337,4 @@ s.replace(
 
 # Use a python runtime which is available in the owlbot post processor here
 # https://github.com/googleapis/synthtool/blob/master/docker/owlbot/python/Dockerfile
-s.shell.run(["nox", "-s", "blacken-3.10"], hide_output=False)
+s.shell.run(["nox", "-s", "blacken"], hide_output=False)
